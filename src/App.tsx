a4a7e4cb3d32efcee09c1e73c250c84e82bc8573
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-31 10:45:48 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1> hello </h1>
+    </main>
+  )
+}
+
+export default App
