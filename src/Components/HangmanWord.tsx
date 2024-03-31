1fe68573d7e47a641143f870fdfb78b63e020507
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-31 22:31:10 +0100

    word to guess styled

--- src/Components/HangmanWord.tsx
+++ src/Components/HangmanWord.tsx
@@ -1,7 +1,13 @@
 export default function HangmanWord() {
+  const wordToGuess = "account"
+  const letters = wordToGuess.split('').map((letter, index) => {
+    return <span className="letters" key={index}>
+      {letter}
+    </span>
+  })
   return (
     <div className="letters--of--word--to--guess">
-      TEST
+      {letters}
     </div>
   )
 }
