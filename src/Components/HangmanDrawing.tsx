245688dc42ed8d8b9e40c089961d386995b36698
Pooja Gurung <pooja_grg07@hotmail.com>
2024-03-31 21:49:28 +0100

    hangman head, arm, body and legs added

--- src/Components/HangmanDrawing.tsx
+++ src/Components/HangmanDrawing.tsx
@@ -1,6 +1,30 @@
 export default function HangmanDrawing() {
+  const HEAD = (
+    <div className="Hangman--head"></div>
+  )
+  const BODY = (
+    <div className="Hangman--body"></div>
+  )
+  const LEFTARM = (
+    <div className="Hangman--left--arm"></div>
+  )
+  const RIGHTARM = (
+    <div className="Hangman--right--arm"></div>
+  )
+  const LEFTLEG = (
+    <div className="Hangman--left--leg"></div>
+  )
+  const RIGHTLEG = (
+    <div className="Hangman--right--leg"></div>
+  )
   return (
     <div className="HangmanDrawing">
+      {HEAD}
+      {BODY}
+      {LEFTARM}
+      {RIGHTARM}
+      {LEFTLEG}
+      {RIGHTLEG}
       <div className="overhang--bar"></div>
       <div className="top--bar"></div>
       <div className="vertical--bar"></div>
