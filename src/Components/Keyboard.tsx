2d513fa7c9599ceae3a8670ea2df394c8856c596
Pooja Gurung <pooja_grg07@hotmail.com>
2024-04-01 11:09:37 +0100

    keyboard added - uppercased alphabets as buttons

--- src/Components/Keyboard.tsx
+++ src/Components/Keyboard.tsx
@@ -1,7 +1,19 @@
 export default function Keyboard() {
+  const KEY = [...Array(26)].map((_, i) =>
+    String.fromCharCode(i + 97).toUpperCase()
+  );
+  // returns ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
   return (
-    <div>
-      <h3> keyword goes here </h3>
+    <div className="keyboard">
+      {KEY.map((letter) => (
+        <button
+          key={letter}
+          className="key--button"
+        >
+          {letter}
+        </button>
+      ))}
     </div>
   )
 }
